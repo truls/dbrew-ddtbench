b7c3e3c2979f15eb3779e483810f3bf87be99077
Truls Asheim <truls@asheim.dk>
2017-03-22 21:24:10 +0100

    Some slight modifications to R analysis script

--- analysis_scripts/ddt_analysis.R
+++ analysis_scripts/ddt_analysis.R
@@ -3,18 +3,23 @@ pdf("ddt_analysis.pdf");
 library("ggplot2");
 library("reshape");
 
-\data <- read.table("ddtbench.out", header=TRUE);
+data <- read.table("ddtbench.out", header=TRUE);
 
 ## exclude some tests
 data <- data[data$testname != "FFT2",];
+data <- data[data$testname != "NAS_LU_x",];
+##data <- data[data$testname != "NAS_LU_y",];
 data <- data[data$testname != "NAS_MG_x",];
 data <- data[data$testname != "NAS_MG_y",];
 data <- data[data$testname != "NAS_MG_z",];
 data <- data[data$testname != "WRF_x_vec",];
 data <- data[data$testname != "WRF_y_vec",];
-data <- data[data$testname != "LAMMPS_atomic",];
-data <- data[data$testname != "LAMMPS_full",];
-
+##data <- data[data$testname != "LAMMPS_atomic",];
+##data <- data[data$testname != "LAMMPS_full",];
+##data <- data[data$testname != "SPECFEM3D_cm",];
+data <- data[data$testname != "SPECFEM3D_oc",];
+data <- data[data$testname != "SPECFEM3d_mt",];
+##data <- data[data$testname != "MILC_su3_zd",];
 
 ## helper functions
 
@@ -59,7 +64,9 @@ pp_data <- aggregate(data.frame(ndata$bytes, ndata$reference_communication),
 testname <- append(testname, rep("Traditional Ping-Pong", length(pp_data[,1])));
 bytes <- append(bytes, pp_data[,1]);
 value <- append(value, (pp_data[,1] / (pp_data[,3] / 2)));
-packMethod <- rep("manual", length(bytes));
+#packMethod <- rep("manual", length(bytes));
+packMethod <- rep("mpi_pack_ddt", length(bytes));
+
 
 testname <- append(testname, as.character(ndata$testname));
 bytes <- append(bytes, ndata$bytes);
@@ -69,13 +76,17 @@ pp_data <- aggregate(data.frame(ndata$bytes, ndata$reference_communication),
 testname <- append(testname, rep("Traditional Ping-Pong", length(pp_data[,1])));
 bytes <- append(bytes, pp_data[,1]);
 value <- append(value, (pp_data[,1] / (pp_data[,3] / 2)));
-packMethod <- append(packMethod, rep("mpi_ddt", length(ndata$bytes) + length(pp_data[,1])));
+#packMethod <- append(packMethod, rep("mpi_pack_ddt", length(ndata$bytes) + length(pp_data[,1])));
+packMethod <- append(packMethod, rep("mpi_pack_ddt_dbrew", length(ndata$bytes) + length(pp_data[,1])));
+
 
 newdf <- data.frame(testname, packMethod, bytes, value);
 
 ## From: http://stackoverflow.com/questions/38895918/value-missing-with-facet-labeller-function/38897092#38897092
 labels_map <- c("manual" = "Manual Packing",
-                "mpi_ddt" = "MPI DDTs")
+                "mpi_ddt" = "MPI DDTs",
+                "mpi_pack_ddt" = "MPI DDT Pack/Unpack",
+                "mpi_pack_ddt_dbrew" = "MPI DDT Pack/Unpack with DBrew")
 
 m <- ggplot(newdf, aes(x=bytes, y=value, color=testname, shape=testname)) +
     facet_grid(. ~ packMethod, scales="free", labeller=as_labeller(labels_map)) +
@@ -87,7 +98,7 @@ m <- ggplot(newdf, aes(x=bytes, y=value, color=testname, shape=testname)) +
     scale_shape_manual(name="Test Name", values=seq(1, length(unique(ndata$testname))+1)) +
     theme(axis.text.x = element_text(angle=90));
 
-print(m);
+##print(m);
 rm(testname, bytes, value, packMethod, pp_data, newdf, m)
 
 ## create new dataframe [testname  method  bytes  id   time] but
@@ -102,11 +113,22 @@ mndata <- melt(ndata, id=c("testname", "packMethod", "bytes"));
 
 ## do not plot reference and mpi_pack_ddt
 mndata <- mndata[mndata$packMethod != "reference", ];
-mndata <- mndata[mndata$packMethod != "mpi_pack_ddt", ];
+#mndata <- mndata[mndata$packMethod != "mpi_pack_ddt", ];
+mndata <- mndata[mndata$packMethod != "mpi_ddt", ];
+mndata <- mndata[mndata$packMethod != "manual", ];
+
 
 ## remove pack/unpack for mpi_ddt method, because it us 0 anyway
-mndata <- mndata[(mndata$packMethod != "mpi_ddt") | (mndata$id != "pack"),];
-mndata <- mndata[(mndata$packMethod != "mpi_ddt") | (mndata$id != "unpack"),];
+##mndata <- mndata[(mndata$packMethod != "mpi_ddt") | (mndata$id != "pack"),];
+##mndata <- mndata[(mndata$packMethod != "mpi_ddt") | (mndata$id != "unpack"),];
+                                        #
+## Remove DBrew rewriting timing... Should be amortized ;)
+mndata <- mndata[(mndata$packMethod != "mpi_pack_ddt_dbrew") | (mndata$id != "dbrew_rewrite"),];
+
+## We aren't interested in DDT create/free overheads at the moment.
+mndata <- mndata[(mndata$id != "ddt_create_overhead"),];
+mndata <- mndata[(mndata$id != "ddt_free_overhead"),];
+
 
 makeplot <- function(name) {
     print(name);
@@ -117,12 +139,12 @@ makeplot <- function(name) {
         labs(title=name) +
         scale_x_continuous('Datasize [Byte]', labels=si_vec) +
         scale_y_continuous('Time [us]') +
-        scale_shape_manual(name="Phase", breaks=c("communication", "ddt_create_overhead", "ddt_free_overhead", "pack", "unpack"),
-                           labels=c("Communication RTT\n (incl. Packing, excl. DDT create)", "DDT Create", "DDT Free", "Pack", "Unpack"),
-                           values=c(21,22,23,24,25)) +
-        scale_color_manual(name="Phase", breaks=c("communication", "ddt_create_overhead", "ddt_free_overhead", "pack", "unpack"),
-                           labels=c("Communication RTT\n (incl. Packing, excl. DDT create)", "DDT Create", "DDT Free", "Pack", "Unpack"),
-                           values=c(2:6)) +
+        scale_shape_manual(name="Phase", breaks=c("communication", "ddt_create_overhead", "ddt_free_overhead", "pack", "unpack", "dbrew_rewrite"),
+                           labels=c("Communication RTT\n (incl. Packing, excl. DDT create)", "DDT Create", "DDT Free", "Pack", "Unpack", "Rewriting"),
+                           values=c(21,22,23,24,25,26)) +
+        scale_color_manual(name="Phase", breaks=c("communication", "ddt_create_overhead", "ddt_free_overhead", "pack", "unpack", "dbrew_rewrite"),
+                           labels=c("Communication RTT\n (incl. Packing, excl. DDT create)", "DDT Create", "DDT Free", "Pack", "Unpack", "Rewriting"),
+                           values=c(2:7)) +
         theme(axis.text.x = element_text(angle=90));
     print(m);
     return(m);
